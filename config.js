945035ed3bf81caf6b2ff8ec8ab04d637afdd4f0
Yaser <yaser.azem@otmail.com>
2021-01-02 01:08:33 +0200

    updating env vars

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URL: 'mongodb+srv://yaser:pWax2iicch6k74O5@cluster0.kflfa.mongodb.net/social-network-mern?retryWrites=true&w=majority',
-    JWT_SECRET_KEY: 'some very very secret key',
-    PORT: 5000
+    MONGO_URL: process.env.MONGO_URL,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    PORT: process.env.PORT
 }
